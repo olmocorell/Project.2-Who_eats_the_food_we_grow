21cb6bc8e4f85628cfde041737238d5450f9aa9b
Amanda Gálvez Corell <agalvezcorell@gmail.com>
2020-02-01 22:02:09 +0100

    Esto tiene muy buena pinta

--- programa.py
+++ programa.py
@@ -1,9 +1,22 @@
+import src.funciones as fun
 import os
 from dotenv import load_dotenv
 import json
 import requests
 import re
 import pandas as pd
-import src.funciones as fun
+resm = ""
 
 
+fun.cargaDataSet()
+fun.filtraPais()
+fun.limpiaDataSet()
+
+item = input(str("Introduzca el elemento a buscar: "))
+year = input("Ahora introduzca el año de la búsqueda: ")
+
+fun.extraeDatos(item,year)
+fun.graficoDatos(item,year)
+hombres = fun.requestINEh(year)
+mujeres = fun.requestINEm(year)
+fun.graficaPoblacion(mujeres,hombres)
--- src/funciones.py
+++ src/funciones.py
@@ -4,13 +4,86 @@ import json
 import requests
 import re
 import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+lista = [17961,17962,17963,17964]
 
 def cargaDataSet():
     global data
-    data = pd.read_csv("FAO.csv",encoding='cp1252')
+    data = pd.read_csv("input/FAO.csv",encoding='cp1252')
     return data
 
 def filtraPais():
     global datasp
     datasp = data[(data["Area"]== "Spain")]
     return datasp
+
+def limpiaDataSet():
+    global datasp
+    for a in lista:
+        datasp = datasp.drop(a)
+    datasp = datasp.drop_duplicates()
+
+def extraeDatos(item,year):
+    global toneladas
+    data_filter = datasp[datasp["Item"] == f"{item}"]
+    toneladas = list(data_filter[f"Y{year}"])
+    return toneladas
+
+def graficoDatos(item,year):
+    data_filter = datasp[datasp["Item"] == f"{item}"]
+    toneladas = list(data_filter[f"Y{year}"])
+    if len(toneladas) == 1:
+        toneladas = [0] + list(data_filter[f"Y{year}"])
+    labels = "Feed","Food"
+    colors = ['lightseagreen','hotpink']
+    explode = (0.1, 0) 
+    plt.pie(toneladas, labels=labels,colors=colors,explode=explode,
+    autopct='%1.1f%%', shadow=True, startangle=140)
+    plt.title(f"{item} en k toneladas en el año {year}")
+    plt.savefig("graficodatos")
+    print(f"{toneladas[0]}k tonnes Feed - {toneladas[1]}k tonnes Food")
+
+
+def requestINEh(year):
+    global resh
+    date = f"{year}0101:{year}1231"
+    codigo = "CP300335"
+    url= f'http://servicios.ine.es/wstempus/js/ES/DATOS_SERIE/{codigo}?date={date}'
+    print(f"Requesting data from {url}")
+    resh = requests.get(url)
+    if resh.status_code != 200:
+        print(resh.text)
+        raise ValueError("Bad Response")
+    return resh.json()
+
+def requestINEm(year):
+    global resm
+    date = f"{year}0101:{year}1231"
+    codigo = "CP300334"
+    url= f'http://servicios.ine.es/wstempus/js/ES/DATOS_SERIE/{codigo}?date={date}'
+    print(f"Requesting data from {url}")
+    resm = requests.get(url)
+    if resm.status_code != 200:
+        print(resm.text)
+        raise ValueError("Bad Response")
+    return resm.json()
+
+def graficaPoblacion(homb,muj):
+    global poblacionm
+    global poblacionh
+    for m in muj['Data']:
+        fecha = datetime.date.fromtimestamp(m['Fecha'] // 1000)
+        poblacionm = m['Valor']
+    for h in homb['Data']:
+        poblacionh = h['Valor']
+    fig = plt.figure(u'Gráfica de barras') # Figure
+    ax = fig.add_subplot(111) # Axes
+    nombres = [f"Mujeres {poblacionm}",f"Hombres {poblacionh}"]
+    datos = [poblacionm,poblacionh]
+    colors = ["darkmagenta", "lightsalmon"]
+    xx = range(len(datos))
+    ax.bar(xx, datos, width=0.8, align='center',color= colors)
+    ax.set_xticks(xx)
+    ax.set_xticklabels(nombres)
+    plt.savefig("graficopoblacion")
